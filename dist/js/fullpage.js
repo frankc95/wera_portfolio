4e2f11e64cde2ddfb394114a1d813045f42ab250
frankc95 <49732467+frankc95@users.noreply.github.com>
2020-09-22 18:48:53 +0100

    First commit

---
+++ dist/js/fullpage.js
@@ -0,0 +1,20 @@
+new fullpage("#myPage", {
+  menu: "#menu",
+  autoScrolling: true,
+  navigation: true,
+  scrollBar: true,
+  anchors: [
+    "home",
+    "education",
+    "licenses",
+    "experience",
+    "volunteer",
+    "contact",
+  ],
+});
+
+const goDown = document.getElementById("goDown");
+
+goDown.addEventListener("click", function () {
+  fullpage_api.moveSectionDown();
+});
---
+++ dist/js/main.js
@@ -0,0 +1,48 @@
+// Select DOM Items
+
+const menuBtn = document.querySelector(".menu-btn");
+const mobileNav = document.querySelector(".mobile-nav");
+const menuBranding = document.querySelector(".menu-branding");
+const menuNav = document.querySelector(".menu-nav");
+
+const navItems = document.querySelectorAll(".nav-item");
+
+//Set Initial State Of Menu
+let showMenu = false;
+
+menuBtn.addEventListener("click", toggleMenu);
+
+function toggleMenu() {
+  if (!showMenu) {
+    menuBtn.classList.add("close");
+    mobileNav.classList.add("show");
+    menuBranding.classList.add("show");
+    menuNav.classList.add("show");
+
+    navItems.forEach((item) => item.classList.add("show"));
+
+    //Set Menu State
+    showMenu = true;
+  } else {
+    menuBtn.classList.remove("close");
+    mobileNav.classList.remove("show");
+    menuBranding.classList.remove("show");
+    menuNav.classList.remove("show");
+
+    navItems.forEach((item) => item.classList.remove("show"));
+
+    //Set Menu State
+    showMenu = false;
+  }
+}
+
+navItems.forEach((item) => item.addEventListener("click", closeMenu));
+
+function closeMenu() {
+  menuBtn.classList.remove("close");
+  mobileNav.classList.remove("show");
+  menuBranding.classList.remove("show");
+  menuNav.classList.remove("show");
+
+  showMenu = false;
+}
