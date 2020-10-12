aa1c5888dc6e4476a1cb1c4361f6bf56b6ae3780
frankc95 <49732467+frankc95@users.noreply.github.com>
2020-10-12 19:59:18 +0100

    Add files via upload

--- dist/js/cards.js
+++ dist/js/cards.js
@@ -2,10 +2,11 @@
 $(".card ul li a").click(function () {
   var itemID = $(this).attr("href");
   $(".card ul").addClass("item_open");
+  $(".closee").addClass("item_open");
   $(itemID).addClass("item_open");
   return false;
 });
-$(".close").click(function () {
-  $(".port, .card ul").removeClass("item_open");
+$(".closee").click(function () {
+  $(".port, .card ul, .closee").removeClass("item_open");
   return false;
 });
